1782bb50c481ec1b518437a88e8c4a72efb55f92
sagnolkamdem <sagnolkamdem721@gmail.com>
2021-03-14 19:36:41 +0100

    creation du projet et configuration de l'application

---
+++ market/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+import yagmail
+
+# Create your views here.
+
+def home(request):
+    send = False
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        message = request.POST['message']
+        
+        # send_mail(
+        #     name,
+        #     message,
+        #     email,
+        #     ['sagnolkamdem721@gmail.com']
+        # )
+        yag = yagmail.SMTP('sagnolkamdem721@gmail.com', 'sncyfredcdgapvvh')
+        yag.send(to = email,
+         subject = 'hi',
+         contents = message)
+        send = True
+        return render(request, '../templates/base.html', locals())
+        
+    return render(request, '../templates/base.html')
